25fada28b92191dc5e65844aa8c28de2dfd071c4
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-12-12 11:31:49 +0000

    fix - changed Data source to allow hosting

--- src/db/typeORM.ts
+++ src/db/typeORM.ts
@@ -9,7 +9,7 @@ const AppDataSource = new DataSource({
     username: process.env.USER,
     password: process.env.PASSWORD,
     database:  process.env.DATABASE,
-    entities: [__dirname + '../../dist/models/*.js'],
+    entities: [__dirname + 'dist/models/userSchema.js'],
     synchronize: true,
     logging: false,
 })
