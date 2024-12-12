b825db0ea5e7f8fb818b5103526342c06121c80a
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-12-12 09:26:56 +0000

    feat - made severless for vercel

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,19 +2,23 @@ import express from 'express';
 import appRouter from '../routes/users';
 import authRouterApp from '../routes/auth';
 import cors from 'cors';
-import {authenticateJWT} from "../middleware/auth";
+import { authenticateJWT } from "../middleware/auth";
+import { VercelRequest, VercelResponse } from '@vercel/node';
 
 const app = express();
+
+// Middleware for JSON parsing and CORS
 app.use(express.json());
-app.use(cors(
-    {
-        origin: 'http://localhost:58792',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true
-    }
-))
-app.use('/api/users',authenticateJWT, appRouter);
+app.use(cors({
+    origin: 'http://localhost:58792',
+    methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
+    credentials: true,
+}));
+// Routes
+app.use('/api/users', authenticateJWT, appRouter);
 app.use('/api/auth', authRouterApp);
 
-
-export default app;
+// Exporting as a Vercel serverless function
+export default function handler(req: VercelRequest, res: VercelResponse) {
+    app(req, res);
+}
