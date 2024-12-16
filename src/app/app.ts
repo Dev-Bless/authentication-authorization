57e5546951ac4c5adc97300e7db3cfa9ba78aa0d
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2024-12-16 09:31:12 +0000

    feat-updated frontend port

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'http://localhost:58792',
+        origin: 'http://localhost:4200',
         methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
         credentials: true
     }
